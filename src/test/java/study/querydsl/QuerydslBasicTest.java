7bb54b28c28beec87429c75519aa4fa751954742
bkbk123a <qjarud32@naver.com>
2023-03-09 00:24:33 +0900

    프로젝션: select 대상 지정

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -436,4 +436,28 @@ public class QuerydslBasicTest {
                 .fetchFirst();
     }
 
+    @Test
+    public void simpleProjection()
+    {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+    }
+
+    @Test
+    public void tupleProjection()
+    {
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username=" + username);
+            System.out.println("age=" + age);
+        }
+    }
 }
