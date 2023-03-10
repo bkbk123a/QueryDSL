612f4acca0c75a86b976c5f2e83c6dc75280a7e2
beomkyung1477 <beomkyung1477@com2us.com>
2023-03-10 09:39:13 +0900

    where절 이용한 다중 파라미터 처리

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -3,6 +3,7 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
@@ -553,8 +554,9 @@ public class QuerydslBasicTest {
         }
     }
 
+    // 동적쿼리
     @Test
-    public void 동적쿼리_BooleanBuilder() throws Exception {
+    public void dynamic_BooleanBuilder() throws Exception {
         String usernameParam = "member1";
         Integer ageParam = 10;
         List<Member> result = searchMember1(usernameParam, ageParam);
@@ -574,5 +576,28 @@ public class QuerydslBasicTest {
                 .where(builder)
                 .fetch();
 
+    }
+
+    @Test
+    public void dynamic_WhereParam() throws Exception {
+        String usernameParam = "member1";
+        Integer ageParam = 10;
+        List<Member> result = searchMember2(usernameParam, ageParam);
+        Assertions.assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember2(String usernameCond, Integer ageCond) {
+        return queryFactory
+                .selectFrom(member)
+                .where(usernameEq(usernameCond), ageEq(ageCond))
+                .fetch();
+    }
+
+    private BooleanExpression usernameEq(String usernameCond) {
+        return usernameCond != null ? member.username.eq(usernameCond) : null;
+    }
+
+    private BooleanExpression ageEq(Integer ageCond) {
+        return ageCond != null ? member.age.eq(ageCond) : null;
     }
 }
