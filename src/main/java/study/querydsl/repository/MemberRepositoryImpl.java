7b2dc6aa03953f972d95bb4283259cb25e260373
bkbk123a <qjarud32@naver.com>
2023-03-12 18:14:58 +0900

    count쿼리 최적화

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -1,13 +1,17 @@
 package study.querydsl.repository;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
+
 import javax.persistence.EntityManager;
 import java.util.List;
 import static org.springframework.util.StringUtils.isEmpty;
@@ -76,7 +80,7 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
         List<MemberTeamDto> content = results.getResults();
         long total = results.getTotal();
-        
+
         return new PageImpl<>(content, pageable, total);
     }
 
@@ -102,16 +106,22 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
         // 전체 카운트를 조회 하는 방법을 최적화 할 수 있으면 이렇게 분리하면 된다. (예를 들어서 전체 카운트를
         // 조회할 때 조인 쿼리를 줄일 수 있다면 상당한 효과가 있다.)
         // 코드를 리펙토링해서 내용 쿼리과 전체 카운트 쿼리를 읽기 좋게 분리하면 좋다.
-        long total = queryFactory
+        JPAQuery<Member> countQuery = queryFactory
                 .select(member)
                 .from(member)
                 .leftJoin(member.team, team)
                 .where(usernameEq(condition.getUsername()),
                         teamNameEq(condition.getTeamName()),
                         ageGoe(condition.getAgeGoe()),
-                        ageLoe(condition.getAgeLoe()))
-                .fetchCount();
+                        ageLoe(condition.getAgeLoe()));
 
-        return new PageImpl<>(content, pageable, total);
+        // return new PageImpl<>(content, pageable, total);
+        return PageableExecutionUtils.getPage(content, pageable,
+                countQuery::fetchCount);
+
+//        스프링 데이터 라이브러리가 제공
+//        count 쿼리가 생략 가능한 경우 생략해서 처리
+//        페이지 시작이면서 컨텐츠 사이즈가 페이지 사이즈보다 작을 때
+//        마지막 페이지 일 때 (offset + 컨텐츠 사이즈를 더해서 전체 사이즈 구함)
     }
 }
